de1f8aabed947af7b25b3c5b8d0c646a4fed610c
Stefan Trajkovikj <stefan@thinker.io>
2023-01-19 14:14:05 +0100

    Small bug fixes

--- src/android/models/DoTransactionMessage.kt
+++ src/android/models/DoTransactionMessage.kt
@@ -4,6 +4,8 @@ class DoTransactionMessage(
     val transactionType : String,
     val amount : Double,
     val currency : String,
-    val orderReference: String) {
+    val orderReference: String,
+    val refundStan: String? = null,
+    val refundDate: String? = null) {
 
 }
